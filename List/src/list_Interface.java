42246ce8d4522f48ccfdab83696b04020c6da1f1
Githendra Gowda <githendragowda6@gmail.com>
2024-07-02 16:45:21 +0530

    List Interface in Java

---
+++ List/src/list_Interface.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class list_Interface {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(6);
+//        System.out.println(list);
+//
+        list.add(0,1);
+//        System.out.println(list);
+//
+        list.add(1,3);
+//        System.out.println(list);
+
+//        list.remove(0);
+//        System.out.println(list);
+//
+//        list.remove(0);
+//        System.out.println(list);
+
+//        System.out.println(list.indexOf(6));
+//        System.out.println(list.indexOf(1));
+//        System.out.println(list.indexOf(3));
+        System.out.println(list.size());
+    }
+}
